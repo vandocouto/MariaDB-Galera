e3f3b3f9b01d92b607515c44dc3150fccec5422e
Evandro Couto <ecouto@icasei.com.br>
2017-04-07 12:21:37 -0300

    primeira versão

--- terraform/deploy.sh
+++ terraform/deploy.sh
@@ -5,8 +5,8 @@ then
   exit 1
 fi
 
-export AWS_ACCESS_KEY_ID="AKIAJNX5KB3GJCW3IYPQ"
-export AWS_SECRET_ACCESS_KEY="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"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
 export AWS_DEFAULT_REGION="us-west-1"
 
 cd $1
